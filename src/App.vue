80c15a82ab94c8c28dcda6f464931bf134ccb4ff
unknown <1037489156@qq.com>
2020-08-06 11:44:25 +0800

    msg

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+
+    <router-view/>
+  <tabbar></tabbar>
+  </div>
+</template>
+
+<script>
+import Tabbar from '@/components/Tabbar'
+export default {
+  components:{
+    Tabbar
+  }
+}
+</script>
+<style lang="scss">
+
+</style>
